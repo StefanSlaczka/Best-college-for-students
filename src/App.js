3ccf829968a2dcc6973460588fa7517651121a41
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2022-08-15 23:34:08 -0400

    Start of the UI
    Will update UI later. Will add hooks later.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+  //hooks
+//Get data form makming a hook for this page
+//make a hook to gain the llocation only for state to not overload the page
+
+const moreThanOneChoice = () => {
+  //collects data from hook and then make table row with the data
+}
+
+const pageanaisan = () => {
+  //makes pages and orgnise it
+}
+
+const getLocastion = () => {
+  //get locaiton form hook and auto fill it in the state dropdown only one time
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div name="top">
+      <h2 name="logo">logo</h2>
+      <div nam="State dropdown">state</div>
+      <button name="getLocation">find My state</button>
+      <div name="college-name search">search</div>
+      <div name="table">
+        <table>
+          <tr name="copy">
+            <th name="cost">criedit pay</th>
+            <th name="jobs">employent %</th>
+            <th name="total">total criedits  </th>
+            <th name="tuff">difcluty</th>
+            <th name="path">path the college choices</th>
+            <th name="time">total time</th>
+          </tr>
+          {/*make a for lop for this  if there is any more colleges*/}
+        </table>
+      </div>
     </div>
   );
 }
